31508ef1fc4fe03ddae3fd696842a6d99ba4a923
Harshal Shinde <162967827+Harshal1326@users.noreply.github.com>
2026-05-14 06:02:06 +0000

    iteration 3

---
+++ 05_iterations/six.js
@@ -0,0 +1,29 @@
+// Filter Map and Reduce
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => (num > 4))
+// console.log(newNums);  // o/p: [ 5, 6, 7, 8, 9, 10 ]
+
+// -----------
+
+const books = [
+    {title: 'Book one', genre: "Fiction", publish: 1981, edition: 2004},
+    {title: 'Book two', genre: "Non - Fiction", publish: 1999, edition: 2007},
+    {title: 'Book three', genre: "History", publish: 1999, edition: 2007},
+    {title: 'Book four', genre: "Non - Fiction", publish: 1989, edition: 2010},
+    {title: 'Book five', genre: "Science", publish: 2009, edition: 2014},
+    {title: 'Book six', genre: "Fiction", publish: 1987, edition: 2010},
+    {title: 'Book seven', genre: "History", publish: 1986, edition: 1996},
+    {title: 'Book eight', genre: "Science", publish: 2011, edition: 2016},
+    {title: 'Book nine', genre: "Non - Fiction", publish: 1981, edition: 1981},
+]
+
+let userbooks = books.filter( (bk) => bk.genre === 'History')
+
+userbooks = books.filter( (bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+
+console.log(userbooks);
+
